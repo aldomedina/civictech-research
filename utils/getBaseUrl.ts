0988a09264236a4ed5615b9d01a058d613b2761a
Aldo <aldo.medina.z@gmail.com>
2024-06-19 10:51:32 -0400

    more testing on base url

--- utils/getBaseUrl.ts
+++ utils/getBaseUrl.ts
@@ -1,12 +1,5 @@
 export function getBaseUrl(): string {
-  let baseUrl: string = ''
-
-  if (process.env.NODE_ENV === 'development') {
-    baseUrl = 'http://localhost:3000'
-  } else {
-    let domain = process.env.NEXT_PUBLIC_VERCEL_URL || ''
-    baseUrl = `https://${domain}`
-  }
-
-  return baseUrl
+  return process.env.NODE_ENV === 'production'
+    ? `https://${process.env.NEXT_PUBLIC_VERCEL_URL}`
+    : 'http://localhost:3000'
 }
