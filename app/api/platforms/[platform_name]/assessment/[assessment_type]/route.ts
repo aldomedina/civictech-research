6964e10d3c6a73a5be94577cd784acbe4b4123ae
Aldo <aldo.medina.z@gmail.com>
2024-07-01 20:29:48 -0400

    get assessment by type API refactorized

--- app/api/platforms/[platform_name]/assessment/[assessment_type]/route.ts
+++ app/api/platforms/[platform_name]/assessment/[assessment_type]/route.ts
@@ -1,26 +1,16 @@
-// /app/api/platforms/[platform_name]/assessments/[assessment_type]/route.ts
-
-import { authOptions } from '@/lib/auth'
-import assessment_placeholder from '@/placeholders/assessment'
-import { getServerSession } from 'next-auth'
+import { getUserAndPlatformIds } from '@/lib/utils'
 import { sql } from '@vercel/postgres'
 
 export async function GET(
   request: Request,
   { params }: { params: { assessment_type: string; platform_name: string } },
 ) {
-  const session = await getServerSession(authOptions)
-  if (!session) {
-    return Response.json({ error: 'Unauthorized' }, { status: 401 })
+  const { userId, platformId, error } = await getUserAndPlatformIds(request, params.platform_name)
+  if (error) {
+    return Response.json({ error }, { status: 401 })
   }
-  // @ts-ignore
-  const userId = session?.user?.id || ''
-  const { platform_name, assessment_type } = params
-  const platformQuery = await sql`SELECT id FROM platforms WHERE slug = ${platform_name}`
-  if (platformQuery.rows.length === 0) {
-    return Response.json({ error: 'Platform not found' }, { status: 404 })
-  }
-  const platformId = platformQuery.rows[0].id
+
+  const { assessment_type } = params
 
   const assessmentsQuery = await sql`
     SELECT a.id, a.type, a.assessment, a.description,
@@ -39,5 +29,4 @@ export async function GET(
     comment: row.comment,
   }))
   return Response.json(assessments)
-  // Response.json({ message: 'Something went wrong' }, { status: 500 })
 }
